90fdb4e02360279cabc6757a4d942d33bb7f6b3c
ipuimar <ipuimar@upo.es>
2024-07-06 18:12:29 +0200

    Subida inicial del proyecto con la conexion a la bbdd postgres

---
+++ admin-service/src/test/java/com/footballscore/adminservice/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.footballscore.adminservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
